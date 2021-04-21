365d6e565fc52fc46b9c087923a1f783af34a541
ArthurHo-CSULB <arthurho4276@gmail.com>
2021-04-20 22:37:17 -0700

    Created FileManagerFrame

---
+++ CECS277Project/src/cecs277/FileManagerFrame.java
@@ -0,0 +1,17 @@
+package cecs277;
+
+/*
+ * @author Arthur Ho
+ */
+
+import javax.swing.JInternalFrame;
+
+public class FileManagerFrame extends JInternalFrame {
+	// TODO: implement code into App
+	
+	public FileManagerFrame(){
+		this.setMaximizable(true);
+		this.setIconifiable(true);
+		
+	}
+}
