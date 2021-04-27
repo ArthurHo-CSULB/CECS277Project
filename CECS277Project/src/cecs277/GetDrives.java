3355b9cc74fa4e7a80ebda1c2a270ff08982ef3f
ArthurHo-CSULB <arthurho4276@gmail.com>
2021-04-27 11:48:35 -0700

    created GetDrives, can be removed later

--- CECS277Project/src/cecs277/GetDrives.java
+++ CECS277Project/src/cecs277/GetDrives.java
@@ -2,6 +2,8 @@ package cecs277;
 
 import java.io.File;
 
+// This may not be necessary to have separately, it can be implemented into another class
+
 public class GetDrives {
 	public static void main(String[] args) {
 		File[] drives = File.listRoots();
